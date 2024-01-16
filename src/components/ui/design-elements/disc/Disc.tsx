1378b79cd4351b1c86ce12eee35669656225e11b
Aleksandra Dorofeeva <crazyleksy@gmail.com>
2024-01-16 21:06:07 +0300

    'mobile response v-1.8'

--- src/components/ui/design-elements/disc/Disc.tsx
+++ src/components/ui/design-elements/disc/Disc.tsx
@@ -1,22 +1,12 @@
 'use client'
-import { Suspense, type FC, useState } from 'react'
-import SkeletonLoader from '../skeleton/SkeletonLoader'
+import { type FC } from 'react'
 import styles from './Disc.module.scss'
 import Image from 'next/image'
 import { getImageUrl } from '@/config/image-url.config'
 
 const Disc: FC<{ src: string }> = ({ src }) => {
-	const [isImageLoading, setIsImageLoading] = useState(false)
-
 	return (
 		<div className={styles.disk}>
-			{isImageLoading && (
-				<SkeletonLoader
-					containerClassName="absolute -top-0.15 left-0 block w-full h-full  overflow-hidden rounded-full"
-					className="w-full h-full rounded-full block"
-					count={1}
-				/>
-			)}
 			<Image
 				width={130}
 				height={130}
@@ -24,8 +14,6 @@ const Disc: FC<{ src: string }> = ({ src }) => {
 				className={styles.img}
 				src={getImageUrl(src)}
 				loading="lazy"
-				onLoadStart={() => setIsImageLoading(true)}
-				onLoadingComplete={() => setIsImageLoading(false)}
 			/>
 		</div>
 	)
